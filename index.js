83bf25e45f0357485eea7546abb33549a3ebf1d1
Titania01 <adegbitebidmus@gmail.com>
2021-04-12 16:03:58 +0100

    feat: 3.3 phonebook backend step3

--- index.js
+++ index.js
@@ -22,6 +22,20 @@ app.get('/info', (req, res) => {
   `)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  console.log(id)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  res.json(person)
+
+  if (person) {
+    res.json(person)
+  }else {
+    res.status(404).end()
+  }
+})
+
 app.listen(9090, () => {
     console.log('Server is running on port 9090')
 })
