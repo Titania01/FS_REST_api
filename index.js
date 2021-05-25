0f1de604c8dfb856f3e7f8bcdb418376e0301eba
Titania01 <adegbitebidmus@gmail.com>
2021-05-25 12:16:23 +0100

    feat: phonebook database mongoose validation

--- index.js
+++ index.js
@@ -134,6 +134,8 @@ const errorHandler = (err, req, res, next) => {
 
   if (err.name === "CastError") {
     return res.status(400).send({ error: "malfortted id" });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({ error: err.message });
   }
   next(err);
 };
--- models/person.js
+++ models/person.js
@@ -1,17 +1,23 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
 const contactSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      minlength: 3,
+      unique: true,
       required: true,
     },
     number: {
       type: String,
+      minlength: 8,
       required: true,
     },
   },
   { timestamps: true }
 );
+contactSchema.plugin(uniqueValidator);
 
 contactSchema.set("toJSON", {
   transform: (document, returnedObj) => {
